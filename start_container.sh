49bf2437a0fb3bd0aea39e1664c13af528a23d27
Aman kushwah <amankushwah451@gmail.com>
2025-01-07 15:49:44 +0530

    python app

--- start_container.sh
+++ start_container.sh
@@ -2,9 +2,5 @@
 set -e
 version=$(date)
 
-
-# docker pull abhishekf5/python-sample-app-demo
-
-# docker run -d -p 5000:5000 abhishekf5/python-sample-app-demo
-docker pull nginx
-docker run -d -p 80:80 nginx
+docker pull abhishekf5/python-sample-app-demo:v1
+docker run -d -p 5000:5000 abhishekf5/python-sample-app-demo:v1
